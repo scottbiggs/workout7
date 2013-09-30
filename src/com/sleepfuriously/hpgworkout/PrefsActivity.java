b6db12f678e7f96f371b245f870ebaf66f0450df
scott-mac <scottmorganbiggs@gmail.com>
2013-09-30 12:12:23 -0500

    - Now the preferences screen matches the look of the rest of the app.
    - Lots of small changes.

--- src/com/sleepfuriously/hpgworkout/PrefsActivity.java
+++ src/com/sleepfuriously/hpgworkout/PrefsActivity.java
@@ -4,9 +4,17 @@
  */
 package com.sleepfuriously.hpgworkout;
 
+import android.app.Dialog;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PrefsActivity extends PreferenceActivity {
 
@@ -19,6 +27,13 @@ public class PrefsActivity extends PreferenceActivity {
 	/** The number of pixels wide (default) for a single Wheel */
 	public static final int DEFAULT_WHEEL_WIDTH = 10;
 
+	//--------------------------------
+	//	Data
+	//--------------------------------
+
+	/** Made external so that we can control a help dialog */
+	protected static Dialog m_dialog = null;
+
 
 	//--------------------------------
 	//	Methods
@@ -27,13 +42,73 @@ public class PrefsActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// Set the volume buttons to control the volume of our sounds
 		// (music).
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
 		// Shows our prefs.xml file.
-		addPreferencesFromResource(R.xml.pref);
+		addPreferencesFromResource(R.xml.pref);	// Load the preferences xml
+		setContentView(R.layout.my_prefs);		// Load the Activity xml (note that
+												// it references via the ListView with
+												// a special ID).
+
+		Button done = (Button) findViewById(R.id.my_prefs_done_butt);
+		done.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				WGlobals.play_short_click();
+				finish();
+			}
+		});
+
+
+		ImageView logo = (ImageView) findViewById(R.id.my_prefs_logo);
+		logo.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				WGlobals.play_help_click();
+
+				if (m_dialog != null) {
+					Log.e(tag, "Starting a new dialog, but m_dialog is NOT null!!");
+					m_dialog.dismiss();
+					m_dialog = null;
+				}
+
+				// Start a new dialog.  Copied from BaseDialogActivity.
+				m_dialog = new Dialog(PrefsActivity.this);
+
+				// This prevents the automatic title area from being made.
+				// And it has to be the first thing, too.
+				m_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+				m_dialog.setContentView(R.layout.dialog_help);
+
+				// Set the volume buttons to control the volume of our sounds
+				// (music).
+				m_dialog.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+				// Fill in the Views (title & msg).
+				TextView title = (TextView) m_dialog.findViewById(R.id.dialog_help_title_tv);
+				title.setText(R.string.my_prefs_help_title);
+
+				TextView msg = (TextView) m_dialog.findViewById(R.id.dialog_help_msg_tv);
+				msg.setText(R.string.my_prefs_help_msg);
+
+				Button ok_butt = (Button) m_dialog.findViewById(R.id.dialog_help_ok_butt);
+				ok_butt.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						WGlobals.play_short_click();
+						m_dialog.dismiss();
+						m_dialog = null;
+					}
+				});
+				m_dialog.show();
+
+			} // onClick(v)
+		});
+
 	} // onCreate (.)
 
 
